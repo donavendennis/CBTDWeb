3b18fcf294f1f71db3ed90e802b80ad757c6fb99
Donaven Dennis <119719733+donavendennis@users.noreply.github.com>
2024-08-08 01:34:45 +0300

    before complete database rest

--- CBTDWeb/Pages/Products/Index.cshtml.cs
+++ CBTDWeb/Pages/Products/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using DataAccess;
+using Infrastucture.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,20 @@ namespace CBTDWeb.Pages.Products
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UnitOfWork _unitOfWork;
+        public IEnumerable<Product> objProductList;
+
+        public IndexModel(UnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+            objProductList = new List<Product>();
         }
+
+        public IActionResult OnGet()
+        {
+            objProductList = _unitOfWork.Product.GetAll();
+            return Page();
+        }
+
     }
 }
